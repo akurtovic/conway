1c9898c475587b1980b182ea4ffbeaeefd2d001b
Amir Kurtovic <amirkurtovic@gmail.com>
2015-07-02 21:44:16 -0500

    Font change and code comments

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ var app = angular.module('conway', []);
 
 app.controller('mainCtrl', ['$scope', function ($scope) {
 
+  // Initialize spped in milliseconds
   $scope.speed = 200;
+
+  // Initialize size of each cell at 15 pixels
   $scope.cellSize = 15;
+
+  // Initialize number of alive cells and generations
   $scope.aliveCells = 0;
   $scope.generations = 0;
+
+  // Start the board in paused state
   $scope.status = 'paused';
 
+  // Main module that contains all of the logic
+  // Revealing module - public methods are exposed in return
   $scope.conway = (function () {
-    // Initialize main private variables 
+
+    // Set colors for dead and alive cells
     var deadColor = '#9899A6';
     var aliveColor = '#5465E5';
+
+    // Declare module state variables 
     var loop,
       grid,
       canvas,
@@ -23,51 +35,57 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
       numberOfCells,
       cellInnerDimension;
 
+    // Initialize canvas
     canvas = document.getElementById('canvas');
     canvasLeft = canvas.offsetLeft;
     canvasTop = canvas.offsetTop;
-
     if (canvas.getContext) {
       ctx = canvas.getContext('2d');
     }
-
-    // TODO: Have to delete and recreate canvas to truly be able
-    // to toggle the cell sizes
     function initCanvas() {
       grid = [];
+      // Set the dimensions of the grid and cells
       gridWidth = 80 * $scope.cellSize;
       cellsPerRow = gridWidth / $scope.cellSize;
       numberOfCells = Math.pow(cellsPerRow, 2);
       cellInnerDimension = $scope.cellSize-1;
     }
 
-    // Constructor function for cells
+    // Constructor function that creates new cells
     function Cell(left, top, width, height, index) {
+      // Set the placement and dimensions of the cell
       this.left = left;
       this.top = top;
       this.width = width;
       this.height = height;
+      // Set the index of the cell in the grid
       this.index = index;
-      this.fillColor = deadColor;
+      // Initialize the cell as dead
       this.alive = false;
+      this.fillColor = deadColor;
+      // Initialize the number of alive neighbors to 0
       this.aliveNeighbors = 0;
     }
 
-    function Draw(shape) {
-      ctx.fillStyle = shape.fillColor;
-      ctx.fillRect(shape.left, shape.top, shape.width, shape.height);
+    // Takes a cell as a parameter and draws a rect in the canvas
+    function Draw(cell) {
+      ctx.fillStyle = cell.fillColor;
+      ctx.fillRect(cell.left, cell.top, cell.width, cell.height);
     }
 
+    // If the board is paused, run() sets window interval and redraws the board
     function run() {
       $scope.status === 'paused' ? loop = window.setInterval(redrawBoard, $scope.speed) : null;
       $scope.status = 'running';
     }
 
+    // Clear window interval and set state to paused
     function pause() {
       clearInterval(loop);
       $scope.status = 'paused';
     }
 
+    // Reset the board to initial state
     function reset() {
       $scope.generations = 0;
       $scope.aliveCells = 0;
@@ -75,15 +93,18 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
       initBoard();
     }
 
+    // Initialize a new board with the randomize flag set to true
     function randomize() {
       initBoard(true);
     }
 
+    // Pause the board and redraw only 1 generation
     function nextStep(){
       pause();
       window.setTimeout(redrawBoard, 300);
     }
 
+    // Lower the window interval period, speeding up the board redraw
     function speedUp() {
       if ($scope.speed > 50) {
         $scope.speed -= 50;
@@ -94,6 +115,7 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
       }
     }
 
+    // Increase the window interval period, slowing down the board redraw
     function slowDown() {
       if ($scope.speed < 800) {
         $scope.speed += 50;
@@ -104,15 +126,21 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
       }
     }
 
+    // TODO: Still not fully functional
     function changeCellSize() {
       ($scope.cellSize === 10) ? $scope.cellSize += 10 : $scope.cellSize -= 10;
       initBoard();
     }
 
+    // Initialize the game board, used on initial load and reset
     function initBoard(randomize) {
       initCanvas();
+
+      // Fill up the canvas with new Cell objects
       for (var xPoint = 0, yPoint = 0, index = 0; index < numberOfCells; index++) {
+        // Create new cell
         var thisShape = new Cell(xPoint, yPoint, cellInnerDimension, cellInnerDimension, index);
+        // If randomize flag is true, randomly assign cells to alive state
         if (randomize) {
           var randNum = Math.floor(Math.random()*17);
           if (randNum <= 2) {
@@ -120,45 +148,64 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
             thisShape.fillColor = aliveColor;
           }
         }
+        // Add the new cell to the grid
         grid.push(thisShape);
+        // Increment x-axis by the width of a standard cell
         xPoint += $scope.cellSize;
+
+        // If x-axis has reached max width of the grid,
+        // move x-axis to beginning of the line and increment
+        // the y-axis by the height of a standard cell
         if (xPoint % gridWidth === 0) {
           xPoint = 0;
           yPoint += $scope.cellSize;
         }
+        // Draw the cell we just created to the canvas
         Draw(grid[index]);
       }
     }
 
+    // Calculates the number of neighboring cells that are alive
     function calcNeighbors() {
+      // Set o to the number of cells in each row
       var o = cellsPerRow;
+      // neighbors is an array containing the locations of all of the cells
+      // that are directly adjacent to the current cell
       var neighbors = [-(o+1),-o,-(o-1),-1,1,o-1,o,o+1];
       var aliveNeighbors = 0;
-      grid.forEach(function(element) {
-        element.aliveNeighbors = 0;
+
+      // Loop through the grid and find alive neighbors for each cell
+      grid.forEach(function(cell) {
+        cell.aliveNeighbors = 0;
+        // Loop through the neighbors array and for each neighbor of the
+        // current cell, check if it is alive
+        // if so, increment aliveNeighbors by 1
         neighbors.forEach(function(x) {
-          var index = element.index + x;
+          var index = cell.index + x;
           if (index > -1){
             if (grid[index] && grid[index].alive === true){
-              element.aliveNeighbors += 1;
+              cell.aliveNeighbors += 1;
             }
           }
         });
       })
     }
 
-    function birth(element) {
+    // Set a cell to alive
+    function birth(cell) {
       $scope.aliveCells += 1;
-      element.alive = true;
-      element.fillColor = aliveColor;
+      cell.alive = true;
+      cell.fillColor = aliveColor;
     }
 
-    function death(element) {
+    // Set a cell to dead
+    function death(cell) {
       $scope.aliveCells -= 1;
-      element.alive = false;
-      element.fillColor = deadColor;
+      cell.alive = false;
+      cell.fillColor = deadColor;
     }
 
+    // redrawBoard is called on each interval or setTimeout
     function redrawBoard(board) {
       calcNeighbors();
       $scope.aliveCells = 0;
@@ -210,6 +257,7 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
 
     }, false);
 
+    // Return public methods, accessible through $scope.conway
     return {
       run: run,
       pause: pause,
@@ -222,5 +270,7 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
       changeCellSize: changeCellSize
     };
 })();
+  
+  // On page load, initialize the board
   $scope.conway.initBoard();
 }])
